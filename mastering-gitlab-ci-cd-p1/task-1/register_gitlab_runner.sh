eb5fc36389c7642bf20adc4f7a2ca94022e4acc7
d.khlebokazov <d.khlebokazov@mycar.kz>
2024-09-23 12:28:15 +0500

    :bug: register_gitlab_runner.sh updated

--- mastering-gitlab-ci-cd-p1/task-1/register_gitlab_runner.sh
+++ mastering-gitlab-ci-cd-p1/task-1/register_gitlab_runner.sh
@@ -24,7 +24,7 @@ while test $# -gt 0; do
       ;;
     --token)
       shift
-      token=`echo $1 | sed -e 's/^[^=]*=//g'`
+      token=$(echo $1 | sed -e 's/^[^=]*=//g')
       sudo gitlab-runner register \
         --non-interactive \
         --url "https://gitlab.com/" \
