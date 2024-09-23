28b9091f4320d0f8af4d126cf99f29513da09b30
d.khlebokazov <d.khlebokazov@mycar.kz>
2024-09-23 22:32:31 +0500

    :bug: register_gitlab_runner.sh fix

--- mastering-gitlab-ci-cd-p1/task-1/register_gitlab_runner.sh
+++ mastering-gitlab-ci-cd-p1/task-1/register_gitlab_runner.sh
@@ -13,8 +13,8 @@ while getopts t:h: flag; do
     t)
       token="${OPTARG}"
       sudo gitlab-runner register --non-interactive --url "https://gitlab.com/" --token "$token" --executor "shell" --description "shell-runner"
-      echo "@reboot nohup sudo gitlab-runner run &" | crontab
-      nohup sudo gitlab-runner run &
+      echo "@reboot sudo runuser -l gitlab-runner -c 'nohup gitlab-runner run &'" | crontab
+      sudo runuser -l gitlab-runner -c 'nohup gitlab-runner run &'
       ;;
     \?)
 			echo "Invalid option: -$OPTARG" >&2
