0b940cfc260dd20128bc091b4794815a6a246d7b
d.khlebokazov <d.khlebokazov@mycar.kz>
2024-09-28 20:37:09 +0500

    :hammer: CI/CD task-1 improvements

--- mastering-gitlab-ci-cd-p1/task-1/cleanup.sh
+++ mastering-gitlab-ci-cd-p1/task-1/cleanup.sh
@@ -1,9 +1,21 @@
 #! /bin/bash
+
+access_token=$1
+
+if [ -z "$access_token" ]; then
+  echo "Please provide the access token"
+  echo "Example:"
+  echo "$0 <access_token>"
+  exit 1
+fi
+
 # Stop the gitlab runner
 sudo gitlab-runner stop
 
 # Unregister the gitlab runner
 sudo gitlab-runner unregister --all-runners
+
+curl --header "PRIVATE-TOKEN: $access_token" "https://gitlab.com/api/v4/runners/all?scope=offline&per_page=100" | jq '.[].id' | xargs -I runner_id curl --request DELETE --header "PRIVATE-TOKEN: $access_token" "https://gitlab.com/api/v4/runners/runner_id"
 
 # Remove the gitlab runner
 sudo gitlab-runner uninstall
--- mastering-gitlab-ci-cd-p1/task-1/register_gitlab_runner.sh
+++ mastering-gitlab-ci-cd-p1/task-1/register_gitlab_runner.sh
@@ -2,29 +2,27 @@
 
 # reference https://docs.gitlab.com/runner/register/index.html
 
-while getopts t:h: flag; do
+link="https://gitlab.com/"
+
+while getopts t:g:h:l: flag; do
   case "$flag" in
     h)
       echo "[options]"
       echo "-h, --help                show brief help"
-      echo "-t                        specify a runner token from GitLab"
+      echo "-t                        specify an access token from GitLab"
+      echo "-g                        specify a group id from GitLab"
+      echo "-l                        specify a GitLab link"
       exit 0
       ;;
-    t)
-      token="${OPTARG}"
-      sudo gitlab-runner register --non-interactive --url "https://gitlab.com/" --token "$token" --executor "shell" --description "shell-runner"
-      sudo gitlab-runner verify
-
-      cat << EOF | sudo tee /home/gitlab-runner/.bash_logout
-# ~/.bash_logout: executed by bash(1) when login shell exits.
-
-# when leaving the console clear the screen to increase privacy
-
-#if [ "$SHLVL" = 1 ]; then
-#    [ -x /usr/bin/clear_console ] && /usr/bin/clear_console -q
-# fi
-EOF
-      exit 0
+    t | g)
+      if "$flag" == "t"; then
+        access_token="${OPTARG}"
+      elif "$flag" == "g"; then
+        project_group="${OPTARG}"
+      fi
+      ;;
+    l)
+      link="${OPTARG}"
       ;;
     \?)
 			echo "Invalid option: -$OPTARG" >&2
@@ -36,5 +34,28 @@ EOF
 		;;
   esac
 done
-echo "Token were not provided. Please provide a token using -t option."
+
+if [ -n "$access_token" ]; then
+  if [ -n "$project_group" ]; then
+      token=$(curl --request POST --header "PRIVATE-TOKEN: $access_token" --data "runner_type=group_type&group_id=$project_group" "https://gitlab.com/api/v4/user/runners")
+      sudo gitlab-runner register --non-interactive --url "$link" --token "$token" --executor "shell" --description "shell-runner"
+      sudo gitlab-runner verify
+
+      cat << EOF | sudo tee /home/gitlab-runner/.bash_logout
+# ~/.bash_logout: executed by bash(1) when login shell exits.
+
+# when leaving the console clear the screen to increase privacy
+
+#if [ "$SHLVL" = 1 ]; then
+#    [ -x /usr/bin/clear_console ] && /usr/bin/clear_console -q
+# fi
+EOF
+exit 0
+  else
+    echo "Project group were not provided. Please provide a project group using -g option."
+    exit 1
+  fi
+fi
+
+echo "Token were not provided. Please provide an access token using -t option."
 exit 1
