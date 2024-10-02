d68d242139e71e0148f090ff9571b8c7dbf53a5b
d.khlebokazov <d.khlebokazov@mycar.kz>
2024-10-02 11:47:04 +0500

    :hammer: changed logrotate_setup.sh

--- linux-assignment/task-4/logrotate_setup.sh
+++ linux-assignment/task-4/logrotate_setup.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 
-if [ -z "$1" ] || [ -z "$2" ]; then
+if [ -z "$1" ]; then
   echo "Please provide the command to run the application"
   echo "Example:"
   echo "$0 'python3 app.py'"
@@ -12,25 +12,32 @@ if ! [ -x "$(command -v logrotate)" ]; then
   sudo apt install logrotate
 fi
 
-sudo bash -c "cat <<EOF > /etc/logrotate.d/task4_logs
-/home/$2/logs/app.log {
+sudo cat <<EOF > /etc/logrotate.d/task4_logs
+/home/logs/app.log {
     maxsize 1M
     rotate 3
-    su $2 $2
+    su root root
     create
     copytruncate
     postrotate
-        echo 'log rotated at $(date)' >> /home/$2/log_rotates.log
+        echo "log rotated at \$(date)" >> /home/log_rotates.log
     endscript
 }
-EOF"
+EOF
 
 if ! sudo grep -Fq "/usr/sbin/logrotate /etc/logrotate.d/task4_logs" /etc/crontab; then
   echo "Adding cron job for logrotate"
-  sudo bash -c "echo '* * * * * /usr/sbin/logrotate /etc/logrotate.d/task4_logs' >> /etc/crontab"
+  sudo bash -c "echo '* * * * * root /usr/sbin/logrotate /etc/logrotate.d/task4_logs' >> /etc/crontab"
   sudo systemctl restart cron
 else
   echo "Cron job already exists"
 fi
 
-$1 > /home/$2/logs/app.log 2>&1
+if [ ! -d /home/logs/ ]; then
+  mkdir /home/logs/
+fi
+if [ ! -f /home/logs/app.log ]; then
+  touch /home/logs/app.log
+fi
+
+$1 >> /home/logs/app.log 2>&1 &
