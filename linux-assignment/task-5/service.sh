29ca05a61bbf0493259f5104ca8eb937da51af34
FIFSAK <anuar200572@gmail.com>
2024-10-03 22:15:57 +0500

    add: task-5

---
+++ linux-assignment/task-5/service.sh
@@ -0,0 +1,36 @@
+#! /bin/bash
+
+sudo addgroup task5group
+sudo useradd -G task5group task5user
+
+app_folder=$1
+command_start=$2
+
+sudo cp -r  $app_folder /app
+sudo chown -R task5user:task5group /app
+
+sudo bash -c "cat <<EOF > /etc/systemd/system/task5.service
+[Unit]
+Description=Task5 Auto Start
+After=network.target
+
+[Service]
+ExecStart=$2
+Restart=always
+User=task5user
+Group=task5group
+WorkingDirectory=/
+StandardOutput=append:/app/task5.log
+StandardError=append:/app/task5.log
+
+
+[Install]
+WantedBy=multi-user.target
+EOF"
+
+sudo systemctl daemon-reload
+sudo systemctl start task5.service
+
+sudo systemctl enable task5.service
+
+sudo systemctl status task5.service
