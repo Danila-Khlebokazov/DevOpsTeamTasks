9db64ae101c005f9b24281e27995a098645bff22
d.khlebokazov <d.khlebokazov@mycar.kz>
2024-10-07 00:36:13 +0500

    :hammer: add /etc/hosts task-10 linux

--- linux-assignment/task-10/add_dns.sh
+++ linux-assignment/task-10/add_dns.sh
@@ -1,17 +1,9 @@
-#!bin/bash
+#! /bin/bash
 
 # https://stackoverflow.com/questions/19339248/append-line-to-etc-hosts-file-with-shell-script
 
-# Do we need to delete same lines?
-if [ -z "$1" ]; then
-  echo "Please write ip_address"
-  echo "Example:"
-  echo "$0 '127.0.0.1 example'"
-  exit 1
-fi
-
-if [ -z "$2" ]; then
-  echo "Please write domain"
+if [ -z "$1" ] || [ -z "$2" ]; then
+  echo "Please write ip_address and domain"
   echo "Example:"
   echo "$0 '127.0.0.1 example'"
   exit 1
