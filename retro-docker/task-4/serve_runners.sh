00fde1233b7dd13cfa072816b45b1ccda23af7f0
d.khlebokazov <d.khlebokazov@mycar.kz>
2024-10-23 02:27:54 +0500

    :hammer: updated gitlab-runner-hub retro-docker task-4

--- retro-docker/task-4/serve_runners.sh
+++ retro-docker/task-4/serve_runners.sh
@@ -5,6 +5,7 @@ RUNNER_IMAGE=$DEFAULT_RUNNER_IMAGE
 
 get_current_projects_ids() {
   projects=($(curl -g --header "PRIVATE-TOKEN: $GITLAB_ACCESS_TOKEN" "https://gitlab.com/api/v4/groups/$GITLAB_GROUP/projects/" | jq '.[].id'))
+  echo "Projects: " "${projects[@]}"
   export projects
 }
 
@@ -17,6 +18,7 @@ get_pending_jobs() {
   pending_jobs_all=0
   for project_id in "${projects[@]}"; do
     pending_jobs_num=$(curl -g --header "PRIVATE-TOKEN: $GITLAB_ACCESS_TOKEN" "https://gitlab.com/api/v4/projects/$project_id/jobs?scope=pending" | jq length);
+    echo "Pending Jobs: $pending_jobs_num"
     pending_jobs_all+=$pending_jobs_num
   done
 }
@@ -25,6 +27,7 @@ get_running_jobs() {
   running_jobs_all=0
   for project_id in "${projects[@]}"; do
     running_jobs_num=$(curl -g --header "PRIVATE-TOKEN: $GITLAB_ACCESS_TOKEN" "https://gitlab.com/api/v4/projects/$project_id/jobs?scope=running" | jq length);
+    echo "Running Jobs: $running_jobs_num"
     running_jobs_all+=$running_jobs_num
   done
 }
