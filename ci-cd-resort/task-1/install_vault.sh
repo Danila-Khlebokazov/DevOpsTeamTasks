7775d27be27dba3f96c6a9059554743f59c35cd8
d.khlebokazov <d.khlebokazov@mycar.kz>
2024-10-24 00:29:53 +0500

    :hammer: task-1 ci-cd-resort

--- ci-cd-resort/task-1/install_vault.sh
+++ ci-cd-resort/task-1/install_vault.sh
@@ -2,6 +2,11 @@
 
 # reference https://developer.hashicorp.com/vault/install
 
+if [ "$EUID" -ne 0 ]
+  then echo "Please run as root"
+  exit
+fi
+
 # check if wget and gpg are installed
 if ! command -v wget &> /dev/null
 then
@@ -19,3 +24,22 @@ fi
 wget -O- https://apt.releases.hashicorp.com/gpg | sudo gpg --dearmor -o /usr/share/keyrings/hashicorp-archive-keyring.gpg
 echo "deb [signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] https://apt.releases.hashicorp.com $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/hashicorp.list
 sudo apt update && sudo apt install vault
+
+systemctl enable vault
+
+systemctl start vault
+
+vault -autocomplete-install
+
+export VAULT_ADDR="https://0.0.0.0:8200"
+export VAULT_TOKEN="1"
+
+echo << EOF >> /etc/vault.d/vault.hcl
+disable_mlock = true
+api_addr      = "https://127.0.0.1:8200"
+cluster_addr  = "https://127.0.0.1:8201"
+EOF
+
+vault status
+
+vault operator init
